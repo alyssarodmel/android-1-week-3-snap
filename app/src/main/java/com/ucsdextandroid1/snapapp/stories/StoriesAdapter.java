e969b457621d1070b98263be32b2aa801759ac38
Alyssa <alyssarodmel04@gmail.com>
2019-06-07 19:53:25 -0700

    new code 6/7

--- app/src/main/java/com/ucsdextandroid1/snapapp/stories/StoriesAdapter.java
+++ app/src/main/java/com/ucsdextandroid1/snapapp/stories/StoriesAdapter.java
@@ -2,10 +2,13 @@ package com.ucsdextandroid1.snapapp.stories;
 
 import android.content.Context;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ucsdextandroid1.snapapp.R;
+import com.ucsdextandroid1.snapapp.chat.ChatItemViewHolder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,37 +24,67 @@ public class StoriesAdapter extends RecyclerView.Adapter {
         items.clear();
 
         //TODO add title item, using context.getString(R.string.stories)) to get the title
+        items.add(new StoriesListItem(context.getString(R.string.stories)));
+        for (Story story : stories) {
+            items.add(new StoriesListItem(story));
+        }
 
         //TODO add all of the story items to the list
 
         //TODO let the adapter know that  the data has changed
+        notifyDataSetChanged();
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         //TODO return the correct view holder for each viewType
-        return null;
-    }
+        switch(viewType) {
+            case StoriesListItem.TYPE_Story:
+                StoryCardViewHolder viewHolder = StoryCardViewHolder.inflate(parent);
+                viewHolder.setStoryItemClickCallback(listener);
+                return viewHolder;
+
+            case StoriesListItem.TYPE_TITLE:
+                return StoriesSectionTitleViewHolder.inflate(parent);
+            default:
+                return StoryCardViewHolder.inflate(parent);
+        }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         //TODO bind the title or the story to the correct view holder
+            if(holder instanceof StoryCardViewHolder)
+                ((StoryCardViewHolder) holder).bind(items.get(position).getStory());
+            else if (holder instanceof StoriesSectionTitleViewHolder)
+                ((StoriesSectionTitleViewHolder) holder).bind(items.get(position).getTitle());
     }
 
     @Override
     public int getItemCount() {
         // TODO return the correct item count
-        return 0;
+        return items.size();
     }
 
     @Override
     public int getItemViewType(int position) {
         //TODO return the correct view type
-        return 0;
+     int type = items.get(position).getType();
+        return type;
     }
 
     //TODO add a method that returns the correct span for each item type.
+        public int getSpanAtIndex(int position){
+            switch(getItemViewType(position)){
+                case StoriesListItem.TYPE_Story:
+                    return 1;
+                case StoriesListItem.TYPE_TITLE:
+                    return 2;
+                default :
+                    return 0;
+
+            }
+        }
 
     //TODO add a custom interface called Callback that extends the click listener defined on the StoriesCardViewHolder
 
@@ -59,6 +92,37 @@ public class StoriesAdapter extends RecyclerView.Adapter {
 
         public static final int TYPE_TITLE = 1;
         public static final int TYPE_CARD = 2;
+        private String title;
+        private Story story;
+        private int type;
+
+        public StoriesListItem(String title) {
+            this.title = title;
+            this.story = null;
+            this.type = TYPE_TITLE;
+        }
+
+        public StoriesListItem(Story story) {
+            this.title = null;
+            this.story = story;
+            this.type = TYPE_Story;
+        }
+
+        public int getType() {
+            return type;
+        }
+
+        public Story getStory() {
+            return story;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+    }
+        public void setOnItemClickCallback(StoryCardViewHolder.StoryCardClickListener listener) {
+            this.listener = listener;
 
     }
 
--- app/src/main/java/com/ucsdextandroid1/snapapp/stories/StoriesFragment.java
+++ app/src/main/java/com/ucsdextandroid1/snapapp/stories/StoriesFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -13,9 +14,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.ucsdextandroid1.snapapp.DataSources;
 import com.ucsdextandroid1.snapapp.R;
+import com.ucsdextandroid1.snapapp.chat.Chat;
+import com.ucsdextandroid1.snapapp.chat.ChatItemViewHolder;
 import com.ucsdextandroid1.snapapp.util.WindowUtil;
 
 import java.util.List;
+
+import javax.sql.DataSource;
 
 /**
  * Created by rjaylward on 4/15/19
@@ -41,17 +46,34 @@ public class StoriesFragment extends Fragment {
         WindowUtil.doOnApplyWindowInsetsToPadding(recyclerView, true, true);
 
         //TODO create a adapter
+        StoriesAdapter adapter = new StoriesAdapter();
 
         //TODO create a grid layout manager with default span of 2 and the SpanSizeLookup for each type
+        GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                return adapter.getSpanAtIndex(position);
+            }
+        });
 
         //TODO set up the recyclerView with the layoutManager and adapter
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
 
         //TODO add a callback to the adapter that calls the method onStoryClicked when the user clicks on the list item
+        adapter.setOnItemClickCallback(new StoryCardViewHolder.StoryCardClickListener() {
+            @Override
+            public void onStoryCardItemClicked(Story story) {
+                onStoryClicked(story);
+            }
+        });
 
         DataSources.getInstance().getStoryCards(new DataSources.Callback<List<Story>>() {
             @Override
             public void onDataFetched(List<Story> data) {
                 //TODO set the data from the DataSource to the adapter
+                adapter.setItems(getContext(), data);
             }
         });
 
@@ -59,6 +81,7 @@ public class StoriesFragment extends Fragment {
     }
 
     private void onStoryClicked(Story story) {
+        Toast.makeText(getContext(), story.getStoryLink(), Toast.LENGTH_SHORT).show();
 
     }
 
